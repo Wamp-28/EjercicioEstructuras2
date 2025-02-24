f6539c8585865df636919c98aa634b32361f745b
MILLER-ai-dot <chaconmilleralejandro@gmail.com>
2025-02-24 17:16:16 -0500

    primera parte de la tarea

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Comandos Git");
-        System.out.println("git status");
-        System.out.println("git add .");
-        System.out.println("git commit -m");
-        System.out.println("git push origin rama");
-        System.out.println("git switch -c rama");
-        System.out.println("git switch rama");
-        System.out.println("git branch");
 
-        System.out.println("Miller Alejandro Chacon Fandiño");
-        System.out.println("Dibujar");
-        System.out.println("Futbol");
-        System.out.println("No tengo Mascotas");
+        //uso del switch antiguo
+        int A = 1;
+        switch (A){
+            case 1: System.out.println("A es igual a 1"); break;
+            case 2: System.out.println("A es igual a 2"); break;
+            case 3: System.out.println("A es igual a 3"); break;
+            default: System.out.println("No se cumplio ningun caso");
+        }
+        //el switch evalua los casos y ejecuta el cual se alinea con la condicion
+        // A al ser igual a 1 nos arroja la impresion del caso 1
 
 
 
