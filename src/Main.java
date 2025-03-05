aa5d8e27781c114a0d1cef9febd64a440efdbea4
CarlosF <carlosdavidforerogutierrez@gmail.com>
2025-03-05 05:22:38 -0500

    EjercicioApuesta

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner teclado = new Scanner(System.in);
+        Random ale = new Random();
+        int max = 3;
+        int min = 1;
+        int d1;
+        int Cn;
+        int Minicial;
+
+        System.out.println("INGRESE UN MONTO INICIAL");
+        Minicial = teclado.nextInt();
+
+        d1 = ale.nextInt((max - min + 1)) + min ;
+
+        do {
+            if (d1==1){
+                Minicial= Minicial * 2;
+                System.out.println("¡SE DUPLICA! SU MONTO ES DE: " + Minicial);
+                System.out.println("QUIERE SEGUIR JUGANDO 1=si 2=no");
+                Cn = teclado.nextInt();
+                if (Cn==1){
+                    System.out.println("JUGANDO");
+                    d1 = ale.nextInt((max - min + 1)) + min ;
+                    continue;
+                } else {
+                    System.out.println("SE RETIRO CON " + Minicial);
+                    break;
+                }
+
+            }else if (d1==2){
+                Minicial= Minicial / 2;
+                System.out.println("¡PIERDE LA MITAD! SU MONTO ES DE: " + Minicial);
+                System.out.println("QUIERE SEGUIR JUGANDO 1=si 2=no");
+                Cn = teclado.nextInt();
+                if (Cn==1){
+                    System.out.println("JUGANDO");
+                    d1 = ale.nextInt((max - min + 1)) + min ;
+                    continue;
+                } else {
+                    System.out.println("SE RETIRO CON " + Minicial);
+                    break;
+                }
+            }else if (d1==3){
+                Minicial = 0;
+                System.out.println("PERDIO");
+
+            }
+
+        }while (Minicial>0);
     }
 }
